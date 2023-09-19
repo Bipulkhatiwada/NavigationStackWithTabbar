5c9c3690ea314c6a5df26e8d5af123709aeb81ec
bipul khatiwada <bipul.khatiwada@swifttech.com.np>
2023-09-19 12:45:21 +0545

    navigation stack added

--- NavigationStackDemo/ContentView.swift
+++ NavigationStackDemo/ContentView.swift
@@ -7,15 +7,119 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+enum Route {
+    case prepare(String)
+    case confirm(String)
+    case success(String)
+}
+
+struct Navigator {
+    static func navigate<T: View>(route: Route, content: () -> T) -> AnyView {
+        switch route {
+        case .prepare(let name):
+            return AnyView(
+                NavigationLink(
+                    destination: PrepareView(name: name),
+                    label: {
+                        content()
+                    }
+                )
+            )
+        case .confirm(let confirmModel):
+            return AnyView(
+                NavigationLink(
+                    destination: ConfirmView(name: confirmModel),
+                    label: {
+                        content()
+                    }
+                )
+            )
+        case .success(let successModel):
+            return AnyView(
+                NavigationLink(
+                    destination: SuccessView(name: successModel),
+                    label: {
+                        content()
+                    }
+                )
+            )
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    let views = ["home", "prepare", "confirm","succes"]
+    var body: some View {
+        NavigationView{
+            VStack {
+                    Navigator.navigate(route: .prepare("preparingg")){
+                        Text("Go to Prepare")
+                            .foregroundColor(Color.red)
+                    }
+                
+            }
+            .padding()
+        }
+    }
+}
+
+struct PrepareView: View {
+    var name: String
+    
+    var body: some View {
+        NavigationView{
+            Form {
+                Text(name)
+                    .font(.largeTitle)
+                    Navigator.navigate(route: .confirm("confirming")){
+                        
+                        Button{
+                            
+                        }label: {
+                            Text("go to Confirm")
+                                .foregroundColor(Color.red)
+                        }
+                        
+                    }
+                
+            }
+        }
+    }
+}
+struct ConfirmView: View {
+    var name: String
+    
+    var body: some View {
+        NavigationView{
+            Form {
+                Text(name)
+                    .font(.largeTitle)
+                Navigator.navigate(route: .success("This is success screen")){
+                    
+                    Button{
+                        
+                    }label: {
+                        Text("go to success")
+                            .foregroundColor(Color.red)
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
+struct SuccessView: View {
+    var name: String
+    
+    var body: some View {
+        NavigationView{
+            Form {
+                Text(name)
+                    .font(.largeTitle)
+                
+            }
+        }
     }
 }
 
