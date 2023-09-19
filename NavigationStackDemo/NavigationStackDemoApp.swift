15e89ae483fe820edfd52196f8c003631f118bcf
bipul khatiwada <bipul.khatiwada@swifttech.com.np>
2023-09-19 10:49:13 +0545

    Initial Commit

---
+++ NavigationStackDemo/NavigationStackDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackDemoApp.swift
+//  NavigationStackDemo
+//
+//  Created by Bipul Khatiwada on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
